966bb39ed003b4e6a3a9f3defd127929b89e13f8
hoshiyu950 <kss-25210147@nsgcl.jp>
2026-05-12 15:50:40 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+const books = [
+  { id: 1, title: '1984', author: 'George Orwell' },
+  { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee' },
+  { id: 3, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' }
+];
+
+app.get('/api/books', (req, res) => {
+  res.json(books);
+});
